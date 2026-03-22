16f2cde5bec01769a75b89e58862630b7207484d
vladi93dev <vladi.sem93@gmail.com>
2026-03-22 17:54:51 +0200

    minor fix

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,23 +9,23 @@ function Nav() {
     }, []);
     
     return (
-        <nav id="nav">
-            <a className="nav-logo" href="#">
-                VK
-            </a>
-            <ul className="nav-links">
-                <li>
-                <a href="#stack">Stack</a>
-                </li>
-                <li>
-                <a href="#projects">Projects</a>
-                </li>
-                <li>
-                <a href="#contact">Contact</a>
-                </li>
-            </ul>
-        </nav>
-    )
+      <nav id="nav">
+        <a className="nav-logo" href="#">
+          VS
+        </a>
+        <ul className="nav-links">
+          <li>
+            <a href="#stack">Stack</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    );
 }
 
 export default Nav;
